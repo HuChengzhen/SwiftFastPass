940e5ec247f4e4a148c66579767aa05d0525ba81
Hu Chengzhen <huchengzhen@gmail.com>
2019-06-06 14:45:46 +0800

    Initial Commit

---
+++ SwiftFastPass/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftFastPass
+//
+//  Created by 胡诚真 on 2019/6/6.
+//  Copyright © 2019 huchengzhen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
